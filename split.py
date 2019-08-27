f4b4c8db4cc6223aded0a41658e562217a43cf95
chenguosen <59959389@qq.com>
2019-08-28 00:11:36 +0800

    99

--- split.py
+++ split.py
@@ -3,4 +3,4 @@
 
 str = "$.msgReq.msgBody.systemTime=20190723095551746"
 print (str.split('='))       # 以空格为分隔符，包含 \n
-#print (str.split('=', 1))  # 以空格为分隔符，分隔成两个
+#######print (str.split('=', 1))  # 以空格为分隔符，分隔成两个
